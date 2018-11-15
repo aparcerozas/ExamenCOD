8a6b940c4f119fb9b936ceafe3664dae910df159
aparcerozas <aparcerozas@victoria11.danielcastelao.org>
2018-11-15 09:57:04 +0100

    lovividoCambioNombres

---
+++ lovivido/src/lovivido/LoVivido.java
@@ -0,0 +1,43 @@
+/*
+ * Examen Refactorizacion
+ * 
+ */
+package lovivido;
+
+import javax.swing.JOptionPane;
+
+public class LoVivido {
+
+public static void main(String[] args) {
+//He cambiado los nombres de todas las variables a nombres significativos de lo que representan
+String nombre;
+String edad;
+
+int meses;
+int dias;
+int horas;
+int anhos;
+
+nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
+edad = JOptionPane.showInputDialog ("Escriba su edad: ");
+ 
+anhos = Integer.parseInt (edad);
+ 
+meses = (anhos*12);
+dias = (anhos*365);
+horas = (dias*24);
+ 
+ JOptionPane.showMessageDialog(null,  "Meses: "  +meses,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE);
+
+
+ JOptionPane.showMessageDialog(null, "Días: " +dias,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+
+
+ JOptionPane.showMessageDialog(null, "Horas: " +horas,"Numero de horas vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+
+   System.exit (0);
+
+}
+   
+
+}
